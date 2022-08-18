3efdfc6cd4f21a66a7ce36945f8d3dc430b33a17
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-18 14:27:43 +0300

    02objects first test

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,11 +1,59 @@
-const student = {
-  'name': 'Dymych',
-  age: 32,
-  isActive: false
+type LocalCityType = {
+  title: string
+  countryTitle: string
 }
 
+type AddressType = {
+  streetTitle: string
+  city: LocalCityType
+}
+
+type TechType = {
+  id: number
+  title: string
+}
+
+type StudentType = {
+  id: number
+  name: string
+  age: number
+  isActive: boolean
+  address: AddressType
+  technologies: Array<TechType>
+}
+
+const student: StudentType = {
+  id: 1,
+  name: 'Dymych',
+  age: 32,
+  isActive: false,
+  address: {
+    streetTitle: 'Surganova 2',
+    city: {
+      title: 'Minsk',
+      countryTitle: 'Belarus',
+    }
+  },
+  technologies: [
+    {
+      id: 1,
+      title: 'HTML'
+    },
+    {
+      id: 2,
+      title: 'CSS'
+    },
+    {
+      id: 3,
+      title: 'JS'
+    },
+  ]
+}
+/*
 const school = {
   name: 'IT-INCUBATOR.BY',
   isOpen: true,
   mentors: [],
 }
+*/
+
